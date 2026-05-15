d411c24761586a4f8a65d5c241f27480120ace56
Karol Nowak <karnowa88@gmail.com>
2026-05-15 15:02:41 +0200

    another attemt to do sth by modyfiying  sth with cors

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,10 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200 
 }));
+app.options('*', cors());
 app.use(cookieParser())
 app.use(express.json());
 
